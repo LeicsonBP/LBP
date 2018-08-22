21e5b904a556146c663e0714530f1dca1a023ac8
Leicson Borges Palhano <leicson.palhano@vidroforte.com.br>
2018-08-22 17:22:20 -0300

    Starting Javascript Programming

---
+++ client/src/js/boot.js
@@ -0,0 +1,4 @@
+import {jQuery} from 'base/jQuery.js';
+import {menu} from 'features/menu.js';
+
+$(".header-links-linksToggle").click(menu._toggleMenu);
---
+++ client/src/js/features/menu.js
@@ -0,0 +1,10 @@
+export class openMenu {
+
+	let links = $(".header-links-list");
+
+	_toggleMenu(){
+		links.slideToggle();
+		console.log("Teste");
+	}
+
+}
